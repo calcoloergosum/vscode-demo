810eecaac4c3ac99518bb4860b38016ae67738e8
Jason Han <thestudentofkyoto@gmail.com>
2020-12-02 12:49:27 +0900

    Sample PR

--- factorial/__init__.py
+++ factorial/__init__.py
@@ -1,13 +1,14 @@
-"""Demo module"""
+"""Module for demo"""
 def factorial(num: int) -> int:
-    """Factorial Function
-    https://en.wikipedia.org/wiki/factorial
+    """calculates factorial
+    https://en.wikipedia.org/wiki/Factorial
     """
-    # Written by @studentofkyoto
     if not isinstance(num, int):
-        raise TypeError
-    if num < 0:
-        raise ValueError(f"Should not be negative, but given {num}")
+        raise TypeError()
+    if num > 100:
+        raise ValueError(f"given {num} is too big")
     if num == 0:
         return 1
+    if num < 0:
+        raise ValueError("Negative value not allowed")
     return num * factorial(num - 1)
--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -1,21 +1,22 @@
-"""Tests basic functionality"""
+"""Tests function implementation"""
 import pytest
 from factorial import factorial
 
 
-def test_factorial():
-    """Test basic behavior"""
+def test_basic_behavior():
+    """test basic behavior"""
+    # n! =n x (n-1) x ... 1
     assert factorial(1) == 1
     assert factorial(2) == 2
-    assert factorial(10) == 3628800
+    assert factorial(10) == 1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10
 
-
-def test_wrong_type():
-    """Test it outputs right type of error"""
+def test_extreme_cases():
+    """Extreme cases"""
+    with pytest.raises(ValueError):
+        assert factorial(10000000000000000)
     with pytest.raises(TypeError):
-        assert factorial(1.5)
-
-def test_wrong_range():
-    """Test it outputs right type of error"""
+        assert factorial(0.5)
+    with pytest.raises(TypeError):
+        assert factorial('a')
     with pytest.raises(ValueError):
         assert factorial(-1)
